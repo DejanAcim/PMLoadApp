4211939e0a356017a417532a266ce926e0c14bc5
dejanacim <dejanacim@gmail.com>
2017-06-25 23:48:11 +0200

    Test some workload views and add home page text

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :loads
+
+  def full_name
+  	[name, surname].join(" ")
+  end
 end
