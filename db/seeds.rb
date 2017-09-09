eeb8a80fed54e7d901f34eddf0360c27e690ce3e
dejanacim <dejanacim@gmail.com>
2017-09-09 16:20:57 +0200

    Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+print "User"
+7.times do
+	User.create!(
+		email: Faker::Internet.email,
+		name: Faker::Name.first_name,
+		surname: Faker::Name.last_name,
+		password: 'password')
+	print "."
+end
