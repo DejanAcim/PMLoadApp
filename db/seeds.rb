98f9e1b29ce6d60fc41291cd243499f904b9a271
dejanacim <dejanacim@gmail.com>
2017-09-10 11:16:34 +0200

    Seed team's daily workload

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+scores = 1..6
+date_range = ((Date.today-30.days)..Date.today).map(&:to_s)
+
 print "User"
 7.times do
 	User.create!(
@@ -15,3 +18,28 @@ print "User"
 		password: 'password')
 	print "."
 end
+puts "Users ready."
+
+print "Creating team's workloads"
+date_range.each do |date|
+	TeamDailyWorkload.create!(
+		day: date,
+		score: rand(scores))
+	print "."
+end
+puts ""
+puts "Workloads ready."
+
+# Not ready yet.
+# --------------------------
+# print "Scores"
+# User.all.each do |u|
+# 	3.times do
+# 		Load.create!(
+# 			score: rand(scores),
+# 			user_id: u,
+# 			date: Date.today)
+# 		print "."
+# 	end
+# end
+# puts "Scores ready."
