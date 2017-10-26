9d95ac5b599260636238a87305e94fa3f7c1a6f6
dejanacim <dejanacim@gmail.com>
2017-10-26 22:26:56 +0200

    Delete more junk code and old test files

--- app/controllers/team_daily_workload_controller.rb
+++ app/controllers/team_daily_workload_controller.rb
@@ -2,13 +2,4 @@ class TeamDailyWorkloadController < ApplicationController
   def chart
     @team_daily_workloads = TeamDailyWorkload.all
   end
-
-  def index
-
-  end
-
-  def confirm
-    # @team_daily_workload = TeamDailyWorkload.create(team_daily_workload_params)
-    # @team_daily_workload.score =
-  end
 end
